b19fd04ffc51f0534841dcde9176e28aec15f432
Cozma Rebeca <cozmacristina12@gmail.com>
2021-03-19 14:14:08 +0200

    SM-2 Implemented the Student model class

--- src/loose/oose/fis/lab/student/manager/model/Student.java
+++ src/loose/oose/fis/lab/student/manager/model/Student.java
@@ -1,5 +1,7 @@
 package loose.oose.fis.lab.student.manager.model;
 
+import java.util.Objects;
+
 public class Student {
     private String firstName;
     private String lastName;
@@ -7,24 +9,64 @@ public class Student {
     private double meanGrade;
 
     public Student(String firstName, String lastName, int age, double meanGrade) {
-        //assignments
+        this.firstName = firstName;
+        this.lastName=lastName;
+        this.age=age;
+        this.meanGrade=meanGrade;
     }
 
     @Override
     public String toString() {
-        //toString generated implementation
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", meanGrade=" + meanGrade +
+                '}';
     }
 
     @Override
     public boolean equals(Object o) {
-        //equals generated implementation
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return age == student.age && Double.compare(student.meanGrade, meanGrade) == 0 && Objects.equals(firstName, student.firstName) && Objects.equals(lastName, student.lastName);
     }
 
     @Override
     public int hashCode() {
-        //hashCode generated implementation
+        return Objects.hash(firstName, lastName, age, meanGrade);
     }
 
-    //generated getters and setters for all fields
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getMeanGrade() {
+        return meanGrade;
+    }
+
+    public void setMeanGrade(double meanGrade) {
+        this.meanGrade = meanGrade;
+    }
 }
